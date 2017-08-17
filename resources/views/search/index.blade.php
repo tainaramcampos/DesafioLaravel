661b76294513fec2249edb0aff0b765d0193e46e
tainaramcampos <tainaramcampos@gmail.com>
2017-08-17 00:13:14 -0300

    Criação da view de busca

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -1,7 +1,22 @@
 
 @extends('layouts.welcome')
 @section('content')
-    <html>
-    Busca
-    </html>
+
+        <form class="form-inline" action="/action_page.php">
+            <div class="form-group">
+                <input type="text" class="form-control">
+            </div>
+            <div class="form-group">
+            <div class="dropdown">
+                    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">CPF
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Nome</a></li>
+                        <li><a href="#">Matrícula</a></li>
+                    </ul>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-default">Buscar</button>
+        </form>
+
 @stop
